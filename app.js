0898f9b8b18994766c2068912999a4fb02faacb4
Pragistyo <new.SOS.hope@outlook.com>
2020-07-06 09:30:42 +0700

    pack json

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require("@babel/register")({
-    presets: [
-      [
-        "@babel/preset-env", {
-          "targets": {
-            "node": "current"
-          }
-        }
-      ]
-  ]
-  });
-  
-  // Import the rest of our application.
-  module.exports = require('./index.js')
+import express  from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import chalk from 'chalk';
+import inpatientsRoutes from './Routes/inpatients'
+import medicalRecordsRoutes from './Routes/medicalRecord'
+import doctorRoutes from './Routes/doctors'
+
+const app = express()
+const port = 3000
+require('dotenv').config();
+
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+const DATABASE_URL = process.env.POSTGRES_URL;
+
+
+app.get('/provisions/api/v1/', (req, res) => res.send('Hello Perovisions!'))
+app.use('/provisions/api/v1/inpatients', inpatientsRoutes)
+app.use('/provisions/api/v1/doctors', doctorRoutes)
+app.use('/provisions/api/v1/medicalRecord', medicalRecordsRoutes)
+
+
+app.listen(port, () => console.log(
+    chalk.blue(`Example app listening on port  ${chalk.yellow(port)}!`)
+))
+
