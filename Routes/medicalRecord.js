4284db6931cbc4d61a09013ded20af1392bbad34
Pragistyo <new.SOS.hope@outlook.com>
2020-07-06 10:25:50 +0700

    change end point

--- Routes/medicalRecord.js
+++ Routes/medicalRecord.js
@@ -9,7 +9,7 @@ router.post('/',medicalRecordsController.create )//post new MedicalRecord
 router.get('/:id', medicalRecordsController.getId)
 // router.put('/:id', );
 router.delete('/:id', medicalRecordsController.remove);
-router.post('/byDatePolyclinic', medicalRecordsController.getDateRangePolyclinic)
+router.post('/byDate/Polyclinic', medicalRecordsController.getDateRangePolyclinic)
 
 
 export default router
