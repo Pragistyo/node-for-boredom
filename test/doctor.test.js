54f81d69b9a8f3cb61848c5da09595532ea1d5b9
Pragistyo <new.SOS.hope@outlook.com>
2020-11-06 20:44:04 +0700

    write test for field type reponse for doctor & inpatient

--- test/doctor.test.js
+++ test/doctor.test.js
@@ -12,6 +12,8 @@ describe(`ENDPOINT: '/provisions/api/v1/doctors' `,() =>{
         // await Promise.resolve()
         let response = await chai.request(app).get('/provisions/api/v1/doctors')
         response.status.should.be.equal(200)
+        response.body.queryName.should.be.equal('All Doctors')
+        response.body.data.should.be.an('array')
     //   assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
@@ -23,6 +25,24 @@ describe(`ENDPOINT: '/provisions/api/v1/doctors/:doctor_id' `,() =>{
           // await Promise.resolve()
           let response = await chai.request(app).get('/provisions/api/v1/doctors/8')
           response.status.should.be.equal(200)
+          response.body.queryName.should.be.equal('DoctorbyId')
+          response.body.data.should.be.an('object')
+          response.body.data.should.have.property('firstname')
+          response.body.data.should.have.property('middlename')
+          response.body.data.should.have.property('lastname')
+          response.body.data.should.have.property('mobilenumber')
+          response.body.data.should.have.property('gender')
+          response.body.data.should.have.property('address1')
+          response.body.data.should.have.property('address2')
+          response.body.data.should.have.property('state')
+          response.body.data.should.have.property('zipcode')
+          response.body.data.should.have.property('birthplace')
+          response.body.data.should.have.property('birthday')
+          response.body.data.should.have.property('nik')
+          response.body.data.should.have.property('certificate')
+          response.body.data.should.have.property('specialization')
+          response.body.data.should.have.property('datecertification')
+          response.body.data.should.have.property('countpatientnumber')
       //   assert.equal([1, 2, 3].indexOf(4), -1);
       });
     });
--- test/inpatient.test.js
+++ test/inpatient.test.js
@@ -12,6 +12,8 @@ describe(`ENDPOINT: '/provisions/api/v1/doctors' ` ,() =>{
         // await Promise.resolve()
         let response = await chai.request(app).get('/provisions/api/v1/inpatients')
         response.status.should.be.equal(200)
+        response.body.queryName.should.be.equal('All Inpatients')
+        response.body.data.should.be.an('array')
     //   assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
@@ -23,6 +25,20 @@ describe(`ENDPOINT: '/provisions/api/v1/doctors/:inpatient_id' `,() =>{
           // await Promise.resolve()
           let response = await chai.request(app).get('/provisions/api/v1/inpatients/6')
           response.status.should.be.equal(200)
+          response.body.queryName.should.be.equal('InpatientbyId')
+          response.body.data.should.be.an('object')
+          response.body.data.should.have.property('firstname')
+          response.body.data.should.have.property('middlename')
+          response.body.data.should.have.property('lastname')
+          response.body.data.should.have.property('mobilenumber')
+          response.body.data.should.have.property('gender')
+          response.body.data.should.have.property('address1')
+          response.body.data.should.have.property('address2')
+          response.body.data.should.have.property('state')
+          response.body.data.should.have.property('zipcode')
+          response.body.data.should.have.property('birthplace')
+          response.body.data.should.have.property('birthday')
+          response.body.data.should.have.property('bloodtype')
       //   assert.equal([1, 2, 3].indexOf(4), -1);
       });
     });
